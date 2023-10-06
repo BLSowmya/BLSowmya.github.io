25533d796bb31fe85a3d44446d86aa248820b3b7
BLSowmya <112118575+BLSowmya@users.noreply.github.com>
2023-10-06 16:29:33 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,7 @@
+// Get the width and height of the window
+var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+// Display the dimensions
+console.log("Window Width: " + windowWidth);
+console.log("Window Height: " + windowHeight);
